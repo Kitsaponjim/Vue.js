199d37e96906e1def7d81efa80fb3ca2159e91c6
Kitsaponjim <98834570+Kitsaponjim@users.noreply.github.com>
2024-05-10 11:58:54 +0700

    add me and edit router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue' // import ได้ 2 แบบ <- แบบที่ 1
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/', //กำหนด path
+    name: 'home', // ประกาศ name space
+    component: HomeView // 
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '',
+    name: 'toolbar',
+    component: () => import('../views/Toolbar.vue'),
+    children: [
+      {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/AboutView.vue') // import ได้ 2 แบบ <- แบบที่ 2
+      },
+      {
+        path: '/me',
+        name: 'me',
+        component: () => import('../views/Me.vue') // import ได้ 2 แบบ <- แบบที่ 2
+      }
+    ]
+  },
+  
+  
 ]
 
 const router = new VueRouter({
---
+++ src/views/Me.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <marquee>
+            <h1>แนะนำตัวเอง</h1>
+        </marquee>
+        <p>ชื่อ: นายกิจศภณ สุทโธ</p>
+        <p>ชื่อเล่น: จิม</p>
+        <p>อายุ: 22</p>
+        <p>จบจากมหาลัย: มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+p {
+    font-size: 20px;
+}
+</style>
